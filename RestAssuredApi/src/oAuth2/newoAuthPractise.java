880b89fccb08d5239e0b08ddf8f9f44cd8b0dd7b
jhemmige <hemmige.jayashree@gmail.com>
2021-01-29 19:28:43 +0530

    this is a new method added in develop envt

--- RestAssuredApi/src/oAuth2/newoAuthPractise.java
+++ RestAssuredApi/src/oAuth2/newoAuthPractise.java
@@ -4,6 +4,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class newoAuthPractise {
+	
+	public void newmethod() {
+		
+		System.out.println("this is a new test method created for github testing");
+	}
 
 	public static void main(String[] args) {
 		System.setProperty("webdriver.chrome.driver","/Users/jayashreehemmige/Downloads/chromedriver");
@@ -13,5 +18,7 @@ public class newoAuthPractise {
 		System.out.println("This is the value4-jayashree4");
 
 	}
+	
+	
 
 }
